e60defe96bb4c1552aa291695f3b317523cbb4af
Sammersonnn <Sammersonnn@gmail.com>
2022-06-23 19:32:16 -0400

    Initial commit

---
+++ src/main/java/net/sammerson/raccoonmod/RaccoonMod.java
@@ -0,0 +1,16 @@
+package net.sammerson.raccoonmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RaccoonMod implements ModInitializer {
+
+	public static final String MOD_ID = "raccoonmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/sammerson/raccoonmod/RaccoonModClient.java
@@ -0,0 +1,10 @@
+package net.sammerson.raccoonmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RaccoonModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
